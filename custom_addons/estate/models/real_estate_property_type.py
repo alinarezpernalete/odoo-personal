d97f5b94a4115f4e75a9c4dadebfa65fe6734d91
Angel Linarez Pernalete <alinarezpernalete@gmail.com>
2024-03-23 08:17:18 -0400

    union con offers_id | count de offers_id y su compute | def para enviar al form de offer

--- custom_addons/estate/models/real_estate_property_type.py
+++ custom_addons/estate/models/real_estate_property_type.py
@@ -8,8 +8,26 @@ class RealEstatePropertyType(models.Model):
     name = fields.Char(required=True)
     property_ids = fields.One2many("real_estate_model", "property_type_id", string="Real Estate Property")
     sequence = fields.Integer('Sequence', default=1, help="Used to order stages. Lower is better.")
+    offer_ids = fields.One2many("real_estate_property_offer", "property_type", string="Real Estate Offer")
+    offer_count = fields.Integer(compute="_compute_offer_count")
 
     _sql_constraints = [
         ('name', 'UNIQUE(name)',
          'The name should be unique.')
     ]
+
+    @api.depends("offer_ids.property_type")
+    def _compute_offer_count(self):
+        for record in self:
+            self.offer_count = self.env['real_estate_property_offer'].search_count([('property_type','=',record.name)])
+
+    def action_view_offer(self):
+        return {
+            'name': ('Offers'),
+            'res_model': 'real_estate_property_offer',
+            'view_mode': 'list,form',
+            'context': {},
+            'domain': [('property_type', '=', self.name)],
+            'target': 'current',
+            'type': 'ir.actions.act_window'
+        }
