0fe9f1dd8a5704a1cd7c5598cfba96e9350ffcca
Angel Linarez Pernalete <alinarezpernalete@gmail.com>
2024-03-28 18:12:47 -0400

    Inclusion de la ruta view/res_users heredada y depends para incluir el res_users de Odoo y refrescar al que hago referencia en el heredado

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -8,10 +8,12 @@
     'data': [
         'security/ir.model.access.csv',
         'views/real_estate_property_offer.xml',
+        'views/res_users.xml',
         'views/real_estate.xml',
         'views/real_estate_property_type.xml',
         'views/real_estate_property_tag.xml',
         'views/real_estate_menu.xml',
     ],
+    'depends': ['base'],
     'license': 'LGPL-3',
 }
