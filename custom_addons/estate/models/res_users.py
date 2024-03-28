7bc6d76bbd06839731f30f1c5767951b4fb12041
Angel Linarez Pernalete <alinarezpernalete@gmail.com>
2024-03-28 18:24:51 -0400

    Creacion de modelo heredado tipo extension donde se hace referencia al modelo 'real_estate_model en el campo 'salesperson' con su respectivo filtro en 'domain'

---
+++ custom_addons/estate/models/res_users.py
@@ -0,0 +1,6 @@
+from odoo import fields, models
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    property_ids = fields.One2many("real_estate_model", "salesperson", string="Properties related", domain="['|', ('state', '=', '1'), ('state', '=', '2')]")
