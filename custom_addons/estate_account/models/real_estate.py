391a6e04c166a16e9448844c73c8daa2e4a7c5be
Angel Linarez Pernalete <angel.linarez@ingeint.com>
2024-05-10 15:33:25 -0400

    Command.create varias lineas de invoice

--- custom_addons/estate_account/models/real_estate.py
+++ custom_addons/estate_account/models/real_estate.py
@@ -1,17 +1,23 @@
-from odoo import fields, models
+from odoo import fields, models, Command
 
 class RealEstateModel(models.Model):
     _inherit = 'real_estate_model'
 
     def sold_property(self):
+        #super().sold_property()
         for record in self:
             buyer = record['buyer'].id
-            result = self.env['account.move'].create(
+            self.env['account.move'].create(
                 {
                     'partner_id': buyer,
                     'move_type': 'out_invoice',
-                    'journal_id': 1
+                    'journal_id': 1,
+                    'invoice_line_ids': [
+                        Command.create({
+                            'name': '6 percent of selling price',
+                            'quantity': 1,
+                            'price_unit': self.selling_price * 0.06
+                        })
+                    ]
                 }
-            )
-        # return super(RealEstateModel, self).sold_property()
-        return result
+            )        
