90c25ae2fe99cc2c784ca86238c286230e65dbaa
Angel Linarez Pernalete <alinarezpernalete@gmail.com>
2024-06-07 11:45:11 -0400

    [fix] change real_estate_model in res_users inherit

--- custom_addons/estate/models/res_users.py
+++ custom_addons/estate/models/res_users.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    property_ids = fields.One2many("real_estate_model", "salesperson", string="Properties related", domain="['|', ('state', '=', '1'), ('state', '=', '2')]")
+    property_ids = fields.One2many("real_estate", "salesperson", string="Properties related", domain="['|', ('state', '=', '1'), ('state', '=', '2')]")
