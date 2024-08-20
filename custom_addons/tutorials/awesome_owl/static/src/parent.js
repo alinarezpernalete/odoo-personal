4aba938dfb748d30e5e2d064397d231249975481
Angel Linarez Pernalete <alinarezpernalete@gmail.com>
2024-08-20 08:10:24 -0400

    [add] cards and props

--- custom_addons/tutorials/awesome_owl/static/src/parent.js
+++ custom_addons/tutorials/awesome_owl/static/src/parent.js
@@ -1,18 +1,25 @@
 /** @odoo-module **/
 
-import { Component, xml } from "@odoo/owl";
+import { Component, xml, useState } from "@odoo/owl";
 import { Playground } from "./playground";
+import { Card } from "./card";
 
 export class Parent extends Component {
     static template = xml`
         <div>
             <Playground/>
-            <Playground/>
-            <Playground/>
-            <Playground/>
-            <Playground/>
+            <Card title="state.a" content="state.b"/>
+            <Card title="state.c" content="state.d"/>
         </div>
     `;
 
-    static components = { Playground };
+    static components = { Playground, Card };
+         
+    state = useState({ 
+        a: "card 1",
+        b: "content of card 1",
+        c: "card 2",
+        d: "content of card 2"
+    });
+    
 }
